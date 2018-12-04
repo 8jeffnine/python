8abc932860a68e3e028b97f7f0eb3e8fbe47cdf6
Jeff Jung <8jeffnine@gmail.com>
2018-12-04 23:28:56 +0900

    python grammar exercise

---
+++ .idea/JumpToPython/02-01.py
@@ -0,0 +1,39 @@
+a = 123
+print(a)
+a = -178
+print(a)
+a= 0
+print(a)
+a=1.2
+print(a)
+a=-3.45
+print(a)
+a=4.24E10
+print(a)
+a = 4.24e-10
+print(a)
+a=0o177
+print(a)
+a=0x8ff
+b=0xABC
+print(a)
+print(b)
+a=3
+b=4
+print(a+b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(7%3)
+print(3%7)
+print(7//4)
+print(-7/4)
+print(-7//4)
+
+# test 1
+print((80+75+55)/3.0)
+# test 2
+print(17//3)
+# test 3
+print(17%3)
+# test 4
---
+++ .idea/JumpToPython/02-02.py
@@ -0,0 +1,66 @@
+print("Hello World")
+print('python is fun')
+print("""Life is too short, You need python""")
+print('''Life is too short, You need python''')
+
+food="Python's favorite food is perl"
+print(food)
+heSay='"Python is very easy." he says.'
+print(heSay)
+multiLine='Life is very short.\nYou need python'
+print(multiLine)
+multiLine='''
+Life is too short
+You need python
+'''
+print(multiLine)
+head="Python"
+tail=" is fun!"
+print(head+tail)
+a="python"
+print(a*2)
+print("="*50)
+print("My Program")
+print("="*50)
+a = "Life is too short, You need Python"
+print(a[3])
+
+print(a[0])
+print(a[12])
+print(a[-1])
+
+print(a[-0])
+print(a[-2])
+print(a[-5])
+b = a[0] + a[1] + a[2] + a[3]
+print(b)
+print(a[0:4])
+print(a[0:3])
+print(a[0:5])
+
+print(a[0:2])
+print(a[5:7])
+print(a[12:17])
+print(a[19:])
+print(a[:17])
+print(a[19:-7])
+
+a = "20010331Rainy"
+year=a[:4]
+day=a[4:8]
+weather=a[8:]
+print(year)
+print(day)
+print(weather)
+
+a = "Pithon"
+print(a[1])
+# a[1]='y' // immutable data-type. thus i can't change letter
+# print(a[1])
+
+print(a[:1]+'y'+a[2:])
+
+print("I eat %d apples." % 3)
+print("I eat %s apples." % "five")
+number = 3
+print("I eat %d apples." % number)
